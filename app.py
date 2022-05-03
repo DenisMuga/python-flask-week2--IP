a8c5bef841e671809f52131358fc9a3cd7b955d2
DenisMuga <denismugah5@gmail.com>
2022-05-03 13:28:44 +0300

    Adding api key

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
+from newsapi import NewsApiClient
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    
+    # Init
+    newsapi = NewsApiClient(api_key='1f06391ab2df4b129f263c5fc5ee6f03')
     
     return render_template('index.html')
 
